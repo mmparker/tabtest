361fd33e9e803ce79059701871d562632cb5c6c3
Matt Parker <mattmparker@gmail.com>
2013-06-26 11:27:07 -0600

    Basic CSS and report-generation infrastructure.

---
+++ generate_report.r
@@ -0,0 +1,20 @@
+
+
+
+
+# Strings ain't factors
+options(stringsAsFactors = FALSE)
+
+
+# Load knitr and markdown to generate the report
+library(knitr)
+library(markdown)
+
+# Knit it
+knit("tabtest.rmd")
+
+markdownToHTML(file = "tabtest.md",
+               output = "tabtest.html",
+               stylesheet = "tabtest.css")
+
+
